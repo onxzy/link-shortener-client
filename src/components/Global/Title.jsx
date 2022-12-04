d22abb766d0c1b2b83fdff73b4972b90e3aa0340
onxzy <contact@onxzy.dev>
2022-12-04 22:23:30 +0100

    correct title

--- src/components/Global/Title.jsx
+++ src/components/Global/Title.jsx
@@ -4,7 +4,7 @@ function Title() {
   return (
     <div className="font-mono">
       <h1 className="font-black text-6xl text-center">
-        <span className="text-red">l.</span>onxzy<span className="text-red">.dev</span>
+        <span className="text-red">links.</span>onxzy<span className="text-red">.dev</span>
       </h1>
       <h2 className="text-center indent-60 font-thin italic text-xl text-[#e0e0eb]">link shortener</h2>
     </div>
